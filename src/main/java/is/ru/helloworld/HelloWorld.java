db3eebc1805e0e20db87fea55a6a3a33cbdce418
KjaSva <kjartan.svanur.h@gmail.com>
2013-10-28 15:31:05 +0000

    Added src directory and HelloWorld.java

---
+++ src/main/java/is/ru/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+
+Ackage is.ru.helloworld;
+
+
+public class HelloWorld {
+    public static void main(String[] args) {
+            World world = new World();
+        System.out.println(world.greet());
+    }
+}
